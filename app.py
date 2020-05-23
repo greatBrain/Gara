de0aa8b9e8f5a90cc169ca6f9beedb9272bdbb94
kira <erickfania@gmail.com>
2020-05-23 16:06:17 -0400

    Trying to handle the user voice, and use an instance of Speech_Recognition

--- app.py
+++ app.py
@@ -5,35 +5,46 @@ import re
 import webbrowser as wbb
 
 
-'''class Assistant: 
-ass = Assistant()    
-ass.say_text(input("write your command:")) '''        
+# Initialize the recognizer  
+rec = sr.Recognizer() 
 
-def say_text(self, text):
+def say_text(command):    
 
-    # Initialize the recognizer  
-    rec = sr.Recognizer()  
+    #Initialize the engine
+    engine = pytt.init()
 
-    self.text = str(text)
-    self.engine = pytt.init()
+    engine.say(text)    
+    engine.runAndWait() 
 
-    self.engine.say(text)    
-    self.engine.runAndWait() 
+while(True):
+    # Exception handling to handle 
+    try:
 
-    while(True):
-        # Exception handling to handle 
-        try:
-
-           # use the microphone as source for input.
-           with sr.Microphone() as source2:
-               # wait for a second to let the recognizer 
-               # adjust the energy threshold based on 
-               # the surrounding noise level
-               rec.adjust_for_ambient_noise(source2, duration=0.2)
+      # use the microphone as source for input.
+      with sr.Microphone() as source2:
+          # wait for a second to let the recognizer 
+          # adjust the energy threshold based on 
+          # the surrounding noise level
+          rec.adjust_for_ambient_noise(source2, duration=0.2)
 
 
-        except expression as identifier:
-            pass
+          #listens for the user's input 
+          audio2 = rec.listen(source2)
+
+          # Using ggogle to recognize audio
+          user_text = rec.recognize_google(audio2)
+          user_text = user_text.lower()
+
+          print("You said" + user_text)
+
+          say_text(user_text)
+
+
+    except sr.RequestError as e:
+        print("Could not request results; {0}".format(e))
+
+    except sr.UnknownValueError:
+        print("Unknown error has occured!")
         
           
           
