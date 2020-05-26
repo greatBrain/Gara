2dbf8087f4eaf23b0fbedbd9d689090f0e3d30ff
greatbrain <erickfania@gmail.com>
2020-05-26 15:41:44 -0400

    getting speech

--- app.py
+++ app.py
@@ -4,40 +4,39 @@ import os
 import re
 import webbrowser as wbb
 
-
-
 class Text_To_Speech:
   ''' def say_text(command):
           #Initialize the engine
           engine = pytt.init()
           engine.say(command)
           engine.runAndWait() '''
-      pass
 
 
 class Speech_To_Text:
-      pass
+      def __init__(self):
+          self.ENG = "en-US"
+          self.SPAN = "es-ES"
 
+          self.rec = sr.Recognizer()
 
-# Initialize the recognizer
-rec = sr.Recognizer()
+      def get_speech(self):
 
-ENG = "en-US"
-SPAN = "es-ES"
+           with sr.Microphone() as source:
 
-with sr.Microphone() as source:
-     print("Speak to me:\n")
+               print("Speak to me:\n")
 
-     #adjust the energy threshold based on the surrounding noise level
-     rec.adjust_for_ambient_noise(source, duration=0.2)
-     audio = rec.listen(source)
+               #adjust the energy threshold based on the surrounding noise level
+               self.rec.adjust_for_ambient_noise(source, duration=0.2)
+               self.audio = self.rec.listen(source)
 
-     try:
-          text = rec.recognize_google(audio, language = ENG)
-          print("You told me: {}".format(text))
+               try:
+                  self.text = self.rec.recognize_google(self.audio, language = self.ENG)
+                  print("You told me: {}".format(self.text))
 
+               except Exception as e:
+                  print("Sorry, could not understand you, try it again!\n")
+               except UnknownValueError:
+                  print("Error don't known")
 
-     except Exception as e:
-         print("Sorry, could not understand you, try it again!\n")
-     except UnknownValueError:
-         print("Error don't known")
+stt = Speech_To_Text()
+stt.get_speech()
