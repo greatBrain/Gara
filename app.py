92af58ed5b076313f462021fbfe597d54ec912e1
greatbrain <erickfania@gmail.com>
2020-05-29 21:04:42 -0400

    creat or open an words file

--- app.py
+++ app.py
@@ -18,42 +18,47 @@ class Speech:
           self.SPAN = "es-ES"
 
           self.rec = sr.Recognizer()
-          #self.text = ''
+
       def get_speech(self):
 
            with sr.Microphone() as source:
-               #self.lang = input("language: es, en\n")
                print("Speak to me:\n")
 
                #adjust the energy threshold based on the surrounding noise level
                self.rec.adjust_for_ambient_noise(source, duration=0.2)
                self.audio = self.rec.listen(source)
 
-               try:
-                  self.text = self.rec.recognize_google(self.audio, language = self.SPAN)
-                  print("You told me: {}".format(self.text))
 
+               try:
+                  self.text = self.rec.recognize_google(self.audio, language = self.ENG).lower()
+                  print("You told me: {}".format(self.text))
                except Exception as e:
                   print("Sorry, could not understand you, try it again!\n")
                except UnknownValueError:
                   print("Error don't known")
 
       def run_command(self):
-           #self.txt = self.text
            self.HTTP = "http://www."
            self.HTTPS = "https://www."
            self.domains = ['.com', '.net', '.org']
            self.page_name = ''
 
-
-           if re.search("Abrir", self.text):
-              if re.search("Facebook", self.text):
-                 try:
+           if re.search("abrir", self.text):
+               if re.search("facebook", self.text):
+                  try:
                      self.page_name = "facebook"
                      self.command = (self.HTTPS + self.page_name + self.domains[0])
                      wbb.open(self.command)
-                 except Exception as e:
+                  except Exception as e:
                      raise e
+
+     def add_to_file(self, word):
+         self.word = open("words.txt", "w+")
+
+         for i in len(word):
+             self.word.write(i)
+
+         self.word.close()
 
 sp = Speech()
 sp.get_speech()
