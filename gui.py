6259fb5fb1955f4f99fff82f0ffc8ab87458dba1
greatbrain <erickfania@gmail.com>
2020-06-05 21:35:19 -0400

    defining user interface window

--- gui.py
+++ gui.py
@@ -1,1 +1,14 @@
+'''GUI for the assistant:
+running on kivy'''
 
+from kivy.app import App
+from kivy.uix.label import Label
+from kivy.uix.button import Button
+
+class main_window(App):
+      
+      def build(self):
+          return Label(text = "Pulse here to start")
+
+
+main_window().run()
